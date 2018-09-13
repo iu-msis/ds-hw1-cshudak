7856661b935c95678fcaa043ba6deb7b8661c967
Connor <cshudak@iu.edu>
2018-09-13 17:17:44 -0400

    Fetch working, button broken

---
+++ public/js/my.app.js
@@ -0,0 +1,37 @@
+var userApp = new Vue({
+  el: '#userProfile',
+  data: {
+      userData: {}
+    },
+
+  methods: {
+
+    fetchUser() {
+        fetch('https://randomuser.me/api/')
+        .then(response => response.json() )
+        // this is the same as .then(function(response) {return response.json()} )
+        .then( json => {this.userData = json.results[0]})
+        .catch(err => {
+          console.log(err);
+
+          })
+        }
+
+
+
+    },
+
+    computed: {
+      age: function() {
+        return moment().diff(this.userData.dob.date,'years')
+      },
+      pretty_date: function() {
+        return moment(this.userData.dob.date).format('YYYY-MM-DD')
+      }
+
+    },
+      created: function() {
+        this.fetchUser();
+        console.log(this.userData);
+  }
+})
