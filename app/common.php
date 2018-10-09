e0a3e1dc528a6603384de3469d58a337a9288a8c
Connor <cshudak@iu.edu>
2018-10-09 12:20:12 -0400

    bug fix

--- app/common.php
+++ app/common.php
@@ -4,6 +4,11 @@ chdir(__DIR__);
 
 set_include_path (__DIR__);
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST'
+&& stripos($_SERVER[CONTENT_TYPE], 'application/json')
+  $_POST = json_decode(file_get_contents('php://input'),
+}
+
 
 
 
