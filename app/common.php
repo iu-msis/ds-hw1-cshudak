3a7ef75ec99b80dce409ef99f82eae5152304bb8
Connor <cshudak@iu.edu>
2018-10-09 12:28:15 -0400

    buggity bug fix

--- app/common.php
+++ app/common.php
@@ -5,11 +5,12 @@ chdir(__DIR__);
 set_include_path (__DIR__);
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST'
-&& stripos($_SERVER[CONTENT_TYPE], 'application/json')
-  $_POST = json_decode(file_get_contents('php://input'),
+&& stripos($_SERVER[CONTENT_TYPE], 'application/json') !== false) {
+  $_POST = json_decode(file_get_contents('php://input'), true);
 }
 
 
 
 
 require 'app/environment.php';
+require 'models/Comment.php';
