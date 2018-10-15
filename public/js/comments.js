803cb46e25d19bf36b8d9e9adacd9557329c884c
Connor <cshudak@iu.edu>
2018-10-15 16:13:30 -0400

    Adjusting js

--- public/js/comments.js
+++ public/js/comments.js
@@ -9,45 +9,47 @@ methods: {
   handleNewComment(e) {
     e.preventDefault();
 
-const s = JSON.stringify (this.commentForm);
-console.log(s);
+  const s = JSON.stringify (this.commentForm);
+  console.log(s);
 
-fetch('api/comment.php', {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json; charset=utf-8"
-  },
-  body: s
-})
-.then(function(response)  {
-  console.log(response);
-  return response.json();
-})
-.then( json => {this.comments.push(json)})
-.catch( err => {
-  console.error('COMMENT POST ERROR');
-  console.error(err);
-})
-
-  this.commentForm = this.fetchEmptyCommentForm();
-},
-
-fetchEmptyCommentForm() {
-  return{
-    comment:null
-  }
-},
-},
-
-
-created() {
-
-  fetch('/api/comment.php')
-  .then( response => response.json() )
-  .then( json => {commentsApp.comments = json;} )
-  .catch( err => {
-    console.log('COMMENT FETCH ERROR');
-    console.log(err);
+  fetch('api/comment.php', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8"
+    },
+    body: s
   })
-}
-})
+  .then(function(response)  {
+    console.log(response);
+    return response.json();
+  })
+  .then( json => {this.comments.push(json)})
+  .catch( err => {
+    console.error('COMMENT POST ERROR');
+    console.error(err);
+  })
+
+    this.commentForm = this.fetchEmptyCommentForm();
+  },
+
+  getComment() {
+    fetch('/api/comment.php')
+    .then( response => response.json() )
+    .then( json => {commentsApp.comments = json;} )
+    .catch( err => {
+      console.log('COMMENT FETCH ERROR');
+      console.log(err)
+    })
+  },
+
+  fetchEmptyCommentForm() {
+    return{
+      comment:null
+    }
+  },
+  }
+
+  created() {
+    this.getcomment();
+  }
+});
