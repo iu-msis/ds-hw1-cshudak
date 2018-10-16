a21887fa37e82b5e0873c33843e2e84b1b9a3bfe
Connor <cshudak@iu.edu>
2018-10-16 09:43:37 -0400

    Re adjusting JS

--- public/js/comments.js
+++ public/js/comments.js
@@ -1,55 +1,106 @@
 var commentsApp = new Vue ({
+
 el: '#commentMain',
+
 data: {
+
   comments: [],
+
   commentForm: {}
-},
+
+  },
+
+
 
 methods: {
+
   handleNewComment(e) {
+
     e.preventDefault();
 
-  const s = JSON.stringify (this.commentForm);
-  console.log(s);
 
-  fetch('api/comment.php', {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json; charset=utf-8"
-    },
-    body: s
-  })
-  .then(function(response)  {
-    console.log(response);
-    return response.json();
-  })
-  .then( json => {this.comments.push(json)})
-  .catch( err => {
-    console.error('COMMENT POST ERROR');
-    console.error(err);
-  })
+
+    const s = JSON.stringify (this.commentForm);
+
+    console.log(s);
+
+
+
+    fetch('api/comment.php', {
+
+      method: "POST",
+
+      headers: {
+
+        "Content-Type": "application/json; charset=utf-8"
+
+      },
+
+      body: s
+
+    })
+
+
+    .then(function(response)  {
+
+      console.log(response);
+
+      return response.json();
+
+    })
+
+    .then( json => {this.comments.push(json)})
+
+    .catch( err => {
+
+      console.error('COMMENT POST ERROR');
+
+      console.error(err);
+
+    })
+
+
 
     this.commentForm = this.fetchEmptyCommentForm();
+
   },
 
-  getComment() {
-    fetch('/api/comment.php')
-    .then( response => response.json() )
-    .then( json => {commentsApp.comments = json;} )
-    .catch( err => {
-      console.log('COMMENT FETCH ERROR');
-      console.log(err)
-    })
-  },
+
 
   fetchEmptyCommentForm() {
-    return{
-      comment:null
+
+  return{
+
+    comment:null
+
     }
-  },
+
   }
 
-  created() {
-    this.getcomment();
-  }
-});
+},
+
+
+
+
+
+created() {
+
+
+
+  fetch('/api/comment.php')
+
+  .then( response => response.json() )
+
+  .then( json => {commentsApp.comments = json;} )
+
+  .catch( err => {
+
+    console.log('COMMENT FETCH ERROR');
+
+    console.log(err);
+
+  })
+
+}
+
+})
